c2bf2a6e0f15a91a00a378dd61519673b5f116e1
Artem Bykov <bykovaa94@mail.ru>
2024-07-19 17:22:05 +0300

    create paralax

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,40 @@
+import clsx from "clsx";
+import cls from "./styles.module.css";
+import { useWindowScroll, useMouse } from "@uidotdev/usehooks";
+
+export const Header = () => {
+  const [{ y }] = useWindowScroll();
+  const [mouse] = useMouse();
+  const xIntersecting = mouse.x;
+  const yIntersecting = mouse.y;
+  Object.assign(document.documentElement, {
+    style: `
+    --scrollTop: ${y}px;
+    --move-x:${(xIntersecting - window.innerWidth / 2) * -.002}deg;
+    --move-y:${(yIntersecting - window.innerHeight / 2) * -.002}deg;
+    `,
+  });
+
+  return (
+    <div className={cls.mainHeader}>
+      <div className={cls.layers}>
+        <div className={cls.layerHeader}>
+          <h1 className={cls.layersCapture}>Welcome to parallax</h1>
+          <h2 className={cls.layerTitle}>Сказочный лес</h2>
+        </div>
+        <div
+          className={clsx(cls.layer, cls.layerBase)}
+          style={{ backgroundImage: "url(./images/layer-base.png)" }}
+        ></div>
+        <div
+          className={clsx(cls.layer, cls.layerMiddle)}
+          style={{ backgroundImage: "url(./images/layer-middle.png)" }}
+        ></div>
+        <div
+          className={clsx(cls.layer, cls.layerFront)}
+          style={{ backgroundImage: "url(./images/layer-front.png)" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/main/Main.tsx
@@ -0,0 +1,24 @@
+// import clsx from "clsx";
+import cls from "./styles.module.css";
+
+export const Main = () => {
+  return (
+    <div
+      className={cls.mainArticle}
+      style={{ backgroundImage: "url(./images/dungeon.jpg)" }}
+    >
+      {/* <div className={cls.mainContent}>
+        <h2 className={cls.mainArticleHeader}>TO BE CONTINUED</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem aperiam
+          pariatur repellat nemo optio corrupti natus, blanditiis doloremque ad
+          esse odio et cum asperiores maiores! Alias, aperiam maxime saepe illum
+          atque consequatur.
+        </p>
+      </div>
+      <div className={cls.copy}>
+        <p>© Изучаю паралакс</p>
+      </div> */}
+    </div>
+  );
+};
